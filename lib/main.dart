f0b3710943044f0366dc86a74948322ccd9289e6
Paul Delhomme <pavel@delhomme.ovh>
2024-09-16 18:05:15 +0200

    Configuration de firebase firestore fonctionnelle normalement

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FirebaseFirestore.instance.clearPersistence();
+
+
   runApp(const MyApp());
 }
 
