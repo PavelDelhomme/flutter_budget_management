178b90bb7c5bf5fd73bdada85f4f0a2b0db3a15e
Paul Delhomme <pavel@delhomme.ovh>
2024-11-08 16:56:14 +0100

    Refactoring: Suppression du temps dans la dates des transactions en ne gardant que "day", "month", "year"

--- lib/models/transaction_model.dart
+++ lib/models/transaction_model.dart
@@ -14,17 +14,17 @@ class Transaction {
   Transaction({
     required this.id,
     required this.user_id,
-    required this.date,
+    required DateTime date,
     required this.notes,
     required this.isRecurring,
     required this.amount,
-  });
+  }) : date = DateTime(date.year, date.month, date.day);
 
   Map<String, dynamic> toMap() {
     return {
       'id': id,
       'user_id': user_id,
-      'date': date,
+      'date': Timestamp.fromDate(DateTime(date.year, date.month, date.day)),
       'notes': notes,
       'isRecurring': isRecurring,
       'amount': amount,
@@ -38,27 +38,18 @@ class Debit extends Transaction {
   GeoPoint localisation;
   String? categorie_id;
 
-  //String? budget_id;
 
   Debit({
-    required String id,
-    required String user_id,
-    required DateTime date,
-    required String notes,
-    required bool isRecurring,
-    required double amount,
+    required super.id,
+    required super.user_id,
+    required super.date,
+    required super.notes,
+    required super.isRecurring,
+    required super.amount,
     this.photos,
     required this.localisation,
     this.categorie_id,
-    //this.budget_id,
-  }) : super(
-          id: id,
-          user_id: user_id,
-          date: date,
-          notes: notes,
-          isRecurring: isRecurring,
-          amount: amount,
-        );
+  });
 
   @override
   Map<String, dynamic> toMap() {
@@ -67,7 +58,6 @@ class Debit extends Transaction {
       'photos': photos ?? [],
       'localisation': localisation,
       'categorie_id': categorie_id,
-      //'budget_id': budget_id,
     });
     return map;
   }
@@ -83,58 +73,38 @@ class Debit extends Transaction {
     return Debit(
       id: map['id'] ?? '',
       user_id: map['user_id'] ?? '',
-      date: (map['date'] as Timestamp).toDate(),
+      date: (map['date'] as Timestamp).toDate().toLocal().subtract(
+          Duration(hours: map['date'].toDate().hour, minutes: map['date'].toDate().minute)),
       notes: map['notes'] ?? '',
       isRecurring: map['isRecurring'] ?? false,
       amount: (map['amount'] as num?)?.toDouble() ?? 0.0,
       photos: List<String>.from(map['photos'] ?? []),
       localisation: map['localisation'] ?? const GeoPoint(0, 0),
-      // Valeur par défaut
       categorie_id: map['categorie_id'],
-      //budget_id: map['budget_id'] ?? '', // Valeur par défaut
     );
   }
 }
 
 // Classe Credit héritant de Transaction, n'ajoutant que le montant spécifique
 class Credit extends Transaction {
-  //String? budget_id;
-
   Credit({
-    required String id,
-    required String user_id,
-    required DateTime date,
-    required String notes,
-    required bool isRecurring,
-    required double amount,
-    //this.budget_id,
-  }) : super(
-          id: id,
-          user_id: user_id,
-          date: date,
-          notes: notes,
-          isRecurring: isRecurring,
-          amount: amount,
-        );
-
-  @override
-  Map<String, dynamic> toMap() {
-    final map = super.toMap();
-    map.addAll({
-      //'budget_id': budget_id,
-    });
-    return map;
-  }
+    required super.id,
+    required super.user_id,
+    required super.date,
+    required super.notes,
+    required super.isRecurring,
+    required super.amount,
+  });
 
   static Credit fromMap(Map<String, dynamic> map) {
     return Credit(
       id: map['id'],
       user_id: map['user_id'],
-      date: (map['date'] as Timestamp).toDate(),
+      date: (map['date'] as Timestamp).toDate().toLocal().subtract(
+          Duration(hours: map['date'].toDate().hour, minutes: map['date'].toDate().minute)), // Supprime l'heure
       notes: map['notes'],
       isRecurring: map['isRecurring'],
       amount: map['amount'],
-      //budget_id: map['budget_id'],
     );
   }
 }
