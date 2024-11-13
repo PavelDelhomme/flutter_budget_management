9bd7a92f7c10139c1ba75899d5ce00e1ee3e712f
PavelDelhomme <pavel@delhomme.ovh>
2024-11-13 17:06:19 +0100

    Restructuration des fonctionnalité pour le test retrospectifs

--- lib/retro_func_check.dart
+++ lib/retro_func_check.dart
@@ -1,74 +1,95 @@
-//todo 1.1. Gestion des Transactions
-//todo 1.1. a. Création et Modification des transactions
-//todo 1.1. b. Formulaire avec sélection du type (crédit/débit) et de la catégorie
-//todo 1.1. c. Ajout d'un label pour le type de transaction (crédit/débit) dans transaction_form_screen.dart
-//todo 1.1. d. Ajout d'une option de récurence pour les transactions.
-//todo 1.1. e. Modification de transcations avec options de suppression ou de récurrence.
-//todo 1.1. f. Mise à jour automatique des transcations récurrentes chaque mois.
-//todo 1.2. Affichage et Gestion des Transactions dans la Liste
-//todo 1.2. a. Affichage trié par date (ordre décroissant)
-//todo 1.2. b. Filtre pour afficher uniquement les transactions récurrentes.
-//todo 1.2. c. Séparation des transactions en sections "récurrentes" et "non récurrentes"
-//todo 1.2. d. Badge d'inndicateur pour les transactions récurrentes.
-//todo 1.2. e. Affichage de la date sous format jour, mois, année (en français).
-//todo 1.2. f. Vue mensuelle et journalière avec options de filtrage par crédits, débits ou tous.
-//todo 1.3. Interaction et Feedback Utilisateur
-//todo 1.3. a. Possibilité de supprimer une transaction par swipe dans transactions_base_view.dart.
-//todo 1.3. b. Possibilité de modifier une transaction par swipe dans transactions_base_view.dart
-//todo 1.3. c. Confirmation de suppression avec dialogue de confirmation pour les transcations réccurentes.
-//todo 1.3. d. Message d'erreur pour les transactions sans catégorie sélectionnée.
-//todo 1.3. e. Sélection de jours avec transctions en surbrillance sur le calendrier.
-//todo 1.3. f. Navigation et rafraîchissement de l'interface après l'ajout ou la modification d'une transaction.
-//todo 1.4. Gestion des Images et des Reçus
-//todo 1.4. a. Limitation de deux reçus par transaction avec option de suppressions.
-//todo 1.4. b. Croix pour supprimer les photos et possibilité de les agrandir dans une vue.
-//todo 1.4. c. Modification ou suppression avec choix de source dans la vue d'une photos agrandie.
-//todo 1.4. d. Intérgation d'une selectieur d'images pour choisir entre galerie et caméra.
-//todo 1.4. e. Galerie de photos pour chaque transactions avec rafraîchissement après modification.
-//todo 2.1. Vue Résumé (SummaryView)
-//todo 2.1. a.
-//todo 3.1. Vue de Transaction (TransactionsBaseView et TransactionDetails)
-//todo 3.1. a.
-//todo 4.1 Gestion des Catégories
-//todo 4.1. a.
-//todo 5.1 Gestion des Budgets
-//todo 5.1. a.
-//todo 6.1 Gestion des Utilisateur et Sécurité
-//todo 6.1. a.
-//todo 7.1 Fonctionnalité de Calendrier
-//todo 7.1. a.
-//todo 8.1 Refactoring du Code en Services
-//todo 8.1. a.
 
+// Gestion des Transactions
+//todo 1.1. Création et Modification des transactions : Gestion des types de transactions (crédit, débit), catégories, récurrence, et options de modification et suppression.
+//todo 1.1.1 Formulaire avec sélection du type (crédit/débit) et de la catégorie
+//todo 1.1.2 Ajout d'un label pour le type de transaction (crédit/débit) dans transaction_form_screen.dart
+//todo 1.1.3 Ajout d'une option de récurrence pour les transactions
+//todo 1.1.4 Modification de transactions avec options de suppression ou de récurrence
+//todo 1.1.5 Mise à jour automatique des transactions récurrentes chaque mois
+
+// Affichage et Gestion des Transactions dans la Liste
+//todo 1.2. Affichage et filtre : Vue structurée des transactions par dates, sections, et types (débit/crédit), avec filtres par récurrence
+//todo 1.2.1 Affichage trié par date (ordre décroissant)
+//todo 1.2.2 Filtre pour afficher uniquement les transactions récurrentes
+//todo 1.2.3 Séparation des transactions en sections "récurrentes" et "non récurrentes"
+//todo 1.2.4 Badge indicateur pour les transactions récurrentes
+//todo 1.2.5 Affichage de la date sous format jour, mois, année (en français)
+//todo 1.2.6 Vue mensuelle et journalière avec options de filtrage par crédits, débits ou tous
+
+// Interaction et Feedback Utilisateur
+//todo 1.3. Interaction avec l’utilisateur : Fonctions de swipe pour modifier ou supprimer, messages de confirmation et d’erreur
+//todo 1.3.1 Suppression d'une transaction par swipe dans transactions_base_view.dart
+//todo 1.3.2 Modification d'une transaction par swipe dans transactions_base_view.dart
+//todo 1.3.3 Confirmation de suppression avec dialogue de confirmation pour les transactions récurrentes
+//todo 1.3.4 Message d'erreur pour les transactions sans catégorie sélectionnée
+//todo 1.3.5 Sélection de jours avec transactions en surbrillance sur le calendrier
+//todo 1.3.6 Navigation et rafraîchissement de l'interface après l'ajout ou la modification d'une transaction
+
+// Gestion des Images et des Reçus
+//todo 1.4. Reçus pour transactions : Limites de reçus par transaction, suppression, agrandissement, et gestion des sources d’image
+//todo 1.4.1 Limitation de deux reçus par transaction avec option de suppression
+//todo 1.4.2 Croix pour supprimer les photos et possibilité de les agrandir dans une vue
+//todo 1.4.3 Modification ou suppression avec choix de source dans la vue d'une photo agrandie
+//todo 1.4.4 Sélecteur d'images pour choisir entre galerie et caméra
+//todo 1.4.5 Galerie de photos pour chaque transaction avec rafraîchissement après modification
+
+// Vue Résumé (SummaryView)
+//todo 2.1 Résumé du budget : Calcul et affichage des totaux de dépenses, crédits, et économies en fonction des transactions en cours
+//todo 2.1.1 Calcul automatique des totaux au chargement de SummaryView
+//todo 2.1.2 Affichage des économies dans SummaryView selon les conditions définies
+//todo 2.1.3 Affichage en temps réel des mises à jour des montants après modification d'une transaction
+
+// Vue de Transaction (TransactionsBaseView et TransactionDetails)
+//todo 3.1 Vue détaillée des transactions : Affichage structuré par jour et par mois avec localisation et options pour voir et supprimer les reçus
+//todo 3.1.1 Vérification de l’affichage des transactions par jour et par mois
+//todo 3.1.2 Confirmation de l'affichage de la date et de la localisation dans transaction_details_view
+//todo 3.1.3 Assurer l'agrandissement des images et la suppression des reçus
+
+// Gestion des Catégories
+//todo 4.1. Gestion des catégories : Sélection, affichage, et vérification de la bonne association des transactions aux catégories
+//todo 4.1.1 Vérification que les transactions débit/crédit ne se mélangent pas entre catégories
+
+// Gestion des Budgets
+//todo 5.1. Gestion des budgets et des totaux mensuels : Affichage et mise à jour automatique du budget en fonction des transactions
+//todo 5.1.1 Tester la copie des transactions récurrentes au changement de mois
+//todo 5.1.2 Vérification de la mise à jour des totaux de budget après ajout ou suppression de transaction
+
+// Gestion des Utilisateurs et Sécurité
+//todo 6.1. Gestion du profil utilisateur : Sécurité des informations personnelles, réauthentification, et restriction d'accès aux champs sensibles
+//todo 6.1.1 Réauthentification pour la modification de mot de passe
+//todo 6.1.2 Restriction d'accès aux champs sensibles (comme le mot de passe) dans le profil
+
+// Fonctionnalité de Calendrier
+//todo 7.1 Calendrier intégré : Affichage des transactions par jour et par mois avec jours marqués en fonction de l’activité
+//todo 7.1.1 Affichage correct du calendrier avec les jours contenant des transactions en surbrillance
+//todo 7.1.2 Tester le fonctionnement du calendrier pour afficher les transactions par jour et par mois
+
+// Refactoring du Code en Services
+//todo 8.1. Refactoring pour organisation en services : Centralisation de la logique commune et optimisation des appels de services
+//todo 8.1.1 Structuration de services pour gérer la logique des transactions, utilisateurs, et catégories
 
 // Checklist pour Test Rétrospectifs
-//todo 1. Création et Modification des transactions
-//todo 1.1. a. Tester la création d'une transaction crédit
-//todo 1.1. b. Tester la création d'une transaction débit
-//todo 1.1. c. Tester la création d'une transaction débit avec récurrence
-//todo 1.1. d. Tester la création d'une transaction crédit avec récurrence
-//todo 1.2. a. Vérifier l'affichage corrects des transactions dans les filtres "récurrentes", "crédits", et "débits",
-//todo 1.2. b. Vérifier la limite de deux reçus par transaction et la possibilité de suppression.
+// Test des transactions
+//todo 1.1 Tester la création et modification de transactions pour chaque cas de figure (crédit, débit, avec ou sans récurrence)
+//todo 1.2 Vérifier l'affichage correct des transactions dans les filtres "récurrentes", "crédits", et "débits"
+//todo 1.3 Vérifier la limitation de deux reçus par transaction et la possibilité de suppression
 
-//todo 2. Gestion des Totaux et Résumé
-//todo 2.1. a. Confirmer le calcul automatique des totaux lors du chargement de SummaryView.
-//todo 2.1. b. Vérifier l’affichage des économies dans SummaryView selon les conditions définies.
-//todo 2.1. c. Tester l'affichage en temps réel des mises à jour des montants après modification d'une transaction.
+// Gestion des Totaux et Résumé
+//todo 2.1 Tester le calcul automatique des totaux lors du chargement de SummaryView et les mises à jour en temps réel
+//todo 2.2 Vérifier l'affichage des économies dans SummaryView selon les conditions définies
 
-//todo 3. Affichage des Transactions et Détails
-//todo 2.1. a. Vérifier l’affichage des transactions par jour et par mois.
-//todo 2.1. b. Confirmer l'affichage de la date et la localisation dans transaction_details_view.
-//todo 2.1. c. Assurer que l’agrandissement des images et la suppression des reçus fonctionnent comme attendu.
+// Affichage des Transactions et Détails
+//todo 3.1 Vérifier l’affichage des transactions par jour et par mois
+//todo 3.2 Confirmer l'affichage de la date et la localisation dans transaction_details_view
+//todo 3.3 Tester l'agrandissement des images et suppression des reçus
 
-//todo 4. Gestion des Budgets
-//todo 2.1. a. Tester la copie des transactions récurrentes au changement de mois.
-//todo 2.1. b. Vérifier la mise à jour des totaux de budget après ajout ou suppression de transaction.
+// Gestion des Budgets
+//todo 4.1 Tester la copie des transactions récurrentes au changement de mois
+//todo 4.2 Vérifier la mise à jour des totaux de budget après ajout ou suppression de transaction
 
-//todo 5. Interface Utilisateur et Sécurité
-//todo 2.1. a. Confirmer la réauthentification pour la modification de mot de passe.
-//todo 2.1. b. Vérifier que seuls les champs nécessaires (mot de passe) sont accessibles dans le profil.
+// Interface Utilisateur et Sécurité
+//todo 5.1 Confirmer la réauthentification pour la modification de mot de passe et accès restreint aux champs nécessaires (mot de passe)
 
-//todo 6. Fonctionnalités du Calendrier
-//todo 2.1. a. Assurer l'affichage correct du calendrier avec les jours contenant des transactions en surbrillance.
-//todo 2.1. b. Tester le fonctionnement du calendrier pour afficher les transactions par jour et par mois.
-
+// Fonctionnalités du Calendrier
+//todo 6.1 Assurer l'affichage correct du calendrier avec les jours contenant des transactions en surbrillance
+//todo 6.2 Tester le fonctionnement du calendrier pour afficher les transactions par jour et par mois
