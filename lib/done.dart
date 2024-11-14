b0ec4c35b5da5f4353493c83e5894e9e161ee8f7
PavelDelhomme <pavel@delhomme.ovh>
2024-11-14 10:50:31 +0100

    Même taille de catégorie pour le champs des catégorie dans le formulaire et refactorisation correcte avec utilisation des champs de formulaire spécifique générer.

--- lib/done.dart
+++ lib/done.dart
@@ -55,3 +55,6 @@
 // au chargement de l'application summary view doit forcément calculere en fonction de ce qu'il se trouve dans la base de données il ne le fait pas actuellement
 
 //transactions_base_view.dart  Failed assertion: line 205 pos 5: 'selectableDayPredicate == null || initialDate == null || selectableDayPredicate(initialDate)': Provided initialDate 2024-11-03 00:00:00.000 must satisfy provided selectableDayPredicate.
+
+//meme taille pour atégorie que autre champs dans formulaire.
+// réccurence en haut dans formulaire
--- lib/todo_files.dart
+++ lib/todo_files.dart
@@ -3,9 +3,4 @@
 // todo Ajoute de l'affichage des prochaine transaction prévu d'ici peu réccurente
 
 
-
-//todo meme taille pour atégorie que autre champs dans formulaire.
-
-//todo réccurence en haut dans formulaire
-
 //todo revérifier les merges pour m'assurer que toutes les fonctionnalité doivent fonctionné
