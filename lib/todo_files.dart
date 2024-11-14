fbe3d34497fa0a9046882033c04d1ded197eeeb0
PavelDelhomme <pavel@delhomme.ovh>
2024-11-14 10:52:31 +0100

    Mise ajour des todos

--- lib/todo_files.dart
+++ lib/todo_files.dart
@@ -4,3 +4,6 @@
 
 
 //todo revérifier les merges pour m'assurer que toutes les fonctionnalité doivent fonctionné
+
+//todo je ne peux plus afficher les détails de la photos lorsque ej clique dessus il faut pourant le faire
+// avec option de suppression
