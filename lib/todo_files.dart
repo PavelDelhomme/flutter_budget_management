632655249ca43876c2f43c64ad1ede141a8695ba
PavelDelhomme <pavel@delhomme.ovh>
2024-11-14 10:50:31 +0100

    Même taille de catégorie pour le champs des catégorie dans le formulaire et refactorisation correcte avec utilisation des champs de formulaire spécifique générer.
    	modifié :         lib/done.dart
    	modifié :         lib/todo_files.dart
    Mergs de tout cela

--- lib/todo_files.dart
+++ lib/todo_files.dart
@@ -3,7 +3,10 @@
 // todo Ajoute de l'affichage des prochaine transaction prévu d'ici peu réccurente
 
 
+<<<<<<< HEAD
 
 
 
+=======
+>>>>>>> b0ec4c3 (Même taille de catégorie pour le champs des catégorie dans le formulaire et refactorisation correcte avec utilisation des champs de formulaire spécifique générer.)
 //todo revérifier les merges pour m'assurer que toutes les fonctionnalité doivent fonctionné
